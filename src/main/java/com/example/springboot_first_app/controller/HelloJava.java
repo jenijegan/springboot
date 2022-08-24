abe96c1419c9a352c1792de7d09f2e989ee96ffc
rajextendo <rajkumar@extendotech.com>
2022-08-24 11:23:15 +0530

    first commit

---
+++ src/main/java/com/example/springboot_first_app/controller/HelloJava.java
@@ -0,0 +1,15 @@
+package com.example.springboot_first_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloJava {
+    @GetMapping("/get")
+    public String Hello(){
+        return "welcome to pubg";
+    }
+    
+
+}
